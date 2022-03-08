e475b6785f6eee4f09c90f64a451185f2318bcce
Igor Spichkin <igorsa.ux@outlook.com>
2022-03-09 02:46:24 +0300

    Tweak imports

--- web/src/hooks/useTranslation.ts
+++ web/src/hooks/useTranslation.ts
@@ -1,6 +1,6 @@
 import { useCallback, useContext } from 'preact/hooks'
-import { LanguageContext } from '../context'
-import TRANSLATION from '../translation'
+import { LanguageContext } from '@context'
+import TRANSLATION from '@/translation'
 
 export default function useTranslation() {
   const { code } = useContext(LanguageContext)
